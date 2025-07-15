664435e936e642ec76e26e798007927500fdc191
NguyenDoQuangDai <22028278@vnu.edu.vn>
2025-07-15 14:40:14 +0700

    Beautify FE with Bootstrap

--- challenge-front-end-live/src/component/Challenge.jsx
+++ challenge-front-end-live/src/component/Challenge.jsx
@@ -1,9 +1,12 @@
 function Challenge({ challenge }) {
     return (
-        <div>
-            <h2>{challenge.name} - {challenge.month}</h2>
-            <p>{challenge.description}</p>
-        </div>
+        <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+            <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{challenge.name}</h5>
+            <small>{challenge.month}</small>
+            </div>
+            <p class="mb-1">{challenge.description}</p>
+        </a>
     );
 }
 
